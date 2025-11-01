c9377633c77d2178b131f253cc9408bafe3bd9d6
Your Name <kgangothri55@gmail.com>
2025-11-01 15:51:26 +0530

    Add navbar to App.js

---
+++ my-reactt-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<nav style={{ backgroundColor: '#333', padding: '10px', color: 'white' }}>
+  <a href="#" style={{ color: 'white', margin: '0 10px' }}>Home</a>
+  <a href="#" style={{ color: 'white', margin: '0 10px' }}>About</a>
+  <a href="#" style={{ color: 'white', margin: '0 10px' }}>Contact</a>
+</nav>
+  );
+}
+
+export default App;
